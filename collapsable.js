60b16ec35780f50ddb603e78354eda455e4642c9
Nick Hargreaves <hargreaves1234@gmail.com>
2021-07-17 02:07:18 +0300

    aunties + uncles + rose

--- collapsable.js
+++ collapsable.js
@@ -2,7 +2,7 @@
     var chart_config = {
         chart: {
             container: "#collapsable-example",
-
+            rootOrientation:  'WEST',
             animateOnInit: true,
             
             node: {
@@ -25,33 +25,115 @@
             image: "img/jim_sue.png",
             children: [
                 {
+                    text:{
+                        name: "Sylvan",
+                        title: "1st born"
+                    },
+                    image: "img/silvan.jpeg",
+                },
+                {
+                    text:{
+                        name: "Mary",
+                        title: "2nd born"
+                    },
                     image: "img/default.png",
+                },
+                {
+                    text:{
+                        name: "Fransisca",
+                        title: "3rd born"
+                    },
+                    image: "img/default.png",
+                },
+                {
+                    text:{
+                        name: "Julita",
+                        title: "4th born"
+                    },
+                    image: "img/julita.jpeg",
+                },
+                {
+                    text:{
+                        name: "Charles",
+                        title: "5th born"
+                    },
+                    image: "img/charles.jpeg",
+                },
+                {
+                    text:{
+                        name: "Esther",
+                        title: "6th born"
+                    },
+                    image: "img/esther.jpeg",
+                },
+                {
+                    text:{
+                        name: "Gaudensia",
+                        title: "7th born"
+                    },
+                    image: "img/default.png",
+                },
+                {
+                    text:{
+                        name: "Rose",
+                        title: "8th born"
+                    },
+                    image: "img/rose.jpeg",
                     collapsed: true,
                     children: [
-                        {
-                            image: "img/default.png"
-                        }
-                    ]
-                },
-                {
-                    image: "img/default.png",
-                    childrenDropLevel: 1,
-                    children: [
-                        {
-                            image: "img/default.png"
-                        }
-                    ]
-                },
-                {
-                    pseudo: true,
-                    children: [
-                        {
-                            image: "img/default.png"
+                           {
+                            text:{
+                                name: "Nick",
+                                title: "1st born"
+                            },
+                            image: "img/nick.jpeg",
+                            children: [
+                                {
+                                    text:{
+                                        name: "Eliana",
+                                        title: "1st born"
+                                    },
+                                    image: "img/default.png",
+                                },
+                                {
+                                    text:{
+                                        name: "Nila",
+                                        title: "2nd born"
+                                    },
+                                    image: "img/default.png",
+                                },
+                            ]
                         },
                         {
-                            image: "img/default.png"
-                        }
+                            text:{
+                                name: "Lillian",
+                                title: "2nd born"
+                            },
+                            image: "img/lillian.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Felix",
+                                title: "3rd born"
+                            },
+                            image: "img/felix.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Maxwell",
+                                title: "4th born"
+                            },
+                            image: "img/maxwell.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Marrianne",
+                                title: "last born"
+                            },
+                            image: "img/marriane.jpeg",
+                        },
                     ]
+
                 }
             ]
         }
