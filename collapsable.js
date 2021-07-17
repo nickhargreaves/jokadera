ca15d84760a7de79a5190f2b3d226fe82e6fdb88
Nick Hargreaves <hargreaves1234@gmail.com>
2021-07-17 03:39:54 +0300

    l3

--- collapsable.js
+++ collapsable.js
@@ -30,6 +30,16 @@
                         title: "1st born"
                     },
                     image: "img/silvan.jpeg",
+                    collapsed: true,
+                    children:[
+                        {
+                            text:{
+                                name: "Allan",
+                                title: "1st born"
+                            },
+                            image: "img/default.png",
+                        }
+                    ]
                 },
                 {
                     text:{
@@ -44,6 +54,61 @@
                         title: "3rd born"
                     },
                     image: "img/default.png",
+                    collapsed: true,
+                    children: [
+                        {
+                            text:{
+                                name: "Otieno",
+                                title: "1st born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Calvin",
+                                title: "2nd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Martin",
+                                title: "3rd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Wicky",
+                                title: "4th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Awino",
+                                title: "5th born"
+                            },
+                            image: "img/awino.jpg",
+                        },{
+                            text:{
+                                name: "Frank",
+                                title: "6th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Orichwe",
+                                title: "7th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Vanessa",
+                                title: "8th born"
+                            },
+                            image: "img/default.png",
+                        },
+                    ]
                 },
                 {
                     text:{
@@ -51,6 +116,48 @@
                         title: "4th born"
                     },
                     image: "img/julita.jpeg",
+                    collapsed: true,
+                    children: [
+                        {
+                            text:{
+                                name: "Marlene",
+                                title: "1st born"
+                            },
+                            image: "img/marlene.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Terry",
+                                title: "2nd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "June",
+                                title: "3rd born"
+                            },
+                            image: "img/june.jpeg",
+                        },{
+                            text:{
+                                name: "Mandella",
+                                title: "4th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Nancy",
+                                title: "5th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Viola",
+                                title: "5th born"
+                            },
+                            image: "img/default.png",
+                        },
+                    ]
                 },
                 {
                     text:{
@@ -58,6 +165,36 @@
                         title: "5th born"
                     },
                     image: "img/charles.jpeg",
+                    collapsed: true,
+                    children: [
+                        {
+                            text:{
+                                name: "Paula",
+                                title: "1st born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Faith",
+                                title: "2nd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Evans",
+                                title: "3rd born"
+                            },
+                            image: "img/evans.jpeg",
+                        },{
+                            text:{
+                                name: "Mike",
+                                title: "4th born"
+                            },
+                            image: "img/default.png",
+                        },
+                    ]
                 },
                 {
                     text:{
@@ -65,6 +202,43 @@
                         title: "6th born"
                     },
                     image: "img/esther.jpeg",
+                    collapsed: true,
+                    children: [
+                        {
+                            text:{
+                                name: "Okomo",
+                                title: "1st born"
+                            },
+                            image: "img/okomo.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Eddy",
+                                title: "2nd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "",
+                                title: "3rd born"
+                            },
+                            image: "img/default.png",
+                        },
+                        {
+                            text:{
+                                name: "Celestine",
+                                title: "4th born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "",
+                                title: "5th born"
+                            },
+                            image: "img/default.png",
+                        },
+                    ]
                 },
                 {
                     text:{
@@ -72,6 +246,36 @@
                         title: "7th born"
                     },
                     image: "img/default.png",
+                    collapsed: true,
+                    children: [
+                        {
+                            text:{
+                                name: "Omondi",
+                                title: "1st born"
+                            },
+                            image: "img/omondi.jpg",
+                        },
+                        {
+                            text:{
+                                name: "Fredrick",
+                                title: "2nd born"
+                            },
+                            image: "img/fredrick.jpeg",
+                        },
+                        {
+                            text:{
+                                name: "Calvin",
+                                title: "3rd born"
+                            },
+                            image: "img/default.png",
+                        },{
+                            text:{
+                                name: "Maxwell",
+                                title: "4th born"
+                            },
+                            image: "img/default.png",
+                        },
+                    ]
                 },
                 {
                     text:{
@@ -87,20 +291,21 @@
                                 title: "1st born"
                             },
                             image: "img/nick.jpeg",
+                            collapsed: true,
                             children: [
                                 {
                                     text:{
                                         name: "Eliana",
                                         title: "1st born"
                                     },
-                                    image: "img/default.png",
+                                    image: "img/eliana.png",
                                 },
                                 {
                                     text:{
                                         name: "Nila",
                                         title: "2nd born"
                                     },
-                                    image: "img/default.png",
+                                    image: "img/nila.png",
                                 },
                             ]
                         },
